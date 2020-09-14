86c79895707ea8f39a5d9f73b3b80467e8416b93
jlynch725 <70283169+jlynch725@users.noreply.github.com>
2020-09-14 14:09:36 -0600

    assignment

--- Basic_Javascript_Projects/Project1_expressions_alert/JS/main.js
+++ Basic_Javascript_Projects/Project1_expressions_alert/JS/main.js
@@ -1,18 +1,20 @@
-var B = "Concatenated" + " String"  //Showing how to concatenate two strings with one variable
-document.write(B)
+var B = "Concatenated" + " String"  //Showing how to concatenate two strings with one variable//
+document.write(B);
 
-var zooanimals = "NYC zoo", mamel = "Lion", reptile = "snake", fish = "shark";  //Assigning multiple varibalbes and writing the string by reference ing the variable
+var zooanimals = "NYC zoo", mamel = "Lion", reptile = "snake", fish = "shark";  //Assigning multiple varibalbes and writing the string by reference ing the variable//
 
 document.write(mamel);
 
-var zooanimals = "NYC zoo";                       // Change the color of one of the variables. 
+var zooanimals = "NYC zoo";                       // Change the color of one of the variables.// 
 var zooanimals = zooanimals.fontcolor("red");
 document.write(zooanimals);
 
-window.alert("This is a popup window");   //create a popup window with a button. 
+window.alert("This is a popup window");   //create a popup window with a button. //
 
-var C = " First wake up and take a shower" //Concatenating by referencing two variables. 
+var C = " First wake up and take a shower" //Concatenating by referencing two variables. //
 var D = "Then eat breakfast"
-document.write(C + D)
+document.write(C + D);
 
-Var E = "weight of car";       //A simple expression, assinging a variable is an example of an expression. 
+Var E = "weight of car";       //A simple expression, assinging a variable is an example of an expression.// 
+
+<button onclick="displayDate()">The time is?</button>
---
+++ Basic_Javascript_Projects/Project2_functions/JS/main.js
@@ -0,0 +1,17 @@
+function change_p_color() {
+var A = "click me"
+var B = A.fontcolor("green");
+var x = document.getElementById("B").innerHTML= B;
+}
+function string_sentences() {
+var sentence = "First wake up";
+sentence += "then take a shower";
+document.getElementById("string_sentences").innerHTML = sentence
+}
+
+
+function myFunction() {
+    var sentence1 = "I am learning";
+    sentence2 = sentence1 + " a lot from this book";
+    document.getElementById("Concatenate").innerHTML = sentence1 + sentence2;
+}
