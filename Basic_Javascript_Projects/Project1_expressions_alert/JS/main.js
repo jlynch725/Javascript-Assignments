662cde8732c8ee4498552add7ed014a0c67bbec9
jlynch725 <70283169+jlynch725@users.noreply.github.com>
2020-09-14 12:31:50 -0600

    JS first assignment

---
+++ Basic_Javascript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,18 @@
+var B = "Concatenated" + " String"  //Showing how to concatenate two strings with one variable
+document.write(B)
+
+var zooanimals = "NYC zoo", mamel = "Lion", reptile = "snake", fish = "shark";  //Assigning multiple varibalbes and writing the string by reference ing the variable
+
+document.write(mamel);
+
+var zooanimals = "NYC zoo";                       // Change the color of one of the variables. 
+var zooanimals = zooanimals.fontcolor("red");
+document.write(zooanimals);
+
+window.alert("This is a popup window");   //create a popup window with a button. 
+
+var C = " First wake up and take a shower" //Concatenating by referencing two variables. 
+var D = "Then eat breakfast"
+document.write(C + D)
+
+Var E = "weight of car";       //A simple expression, assinging a variable is an example of an expression. 
