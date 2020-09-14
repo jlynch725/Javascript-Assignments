03fc0cda55553e284e8401d377e4674401a4250c
jlynch725 <70283169+jlynch725@users.noreply.github.com>
2020-09-14 14:14:26 -0600

    Update main.js

--- Basic_Javascript_Projects/Project2_functions/JS/main.js
+++ Basic_Javascript_Projects/Project2_functions/JS/main.js
@@ -4,14 +4,14 @@ var B = A.fontcolor("green");
 var x = document.getElementById("B").innerHTML= B;
 }
 function string_sentences() {
-var sentence = "First wake up";
-sentence += "then take a shower";
+var sentence1 = "First wake up";
+sentence2 = sentence1 + "then take a shower";
 document.getElementById("string_sentences").innerHTML = sentence
 }
 
 
-function myFunction() {
-    var sentence1 = "I am learning";
-    sentence2 = sentence1 + " a lot from this book";
-    document.getElementById("Concatenate").innerHTML = sentence1 + sentence2;
+function myFunction() {                                 //Defining the function///
+    var sentence1 = "I am learning";                   
+    sentence2 = sentence1 + " a lot from this book";        //the two elements of the string we are putting together.//
+    document.getElementById("Concatenate").innerHTML = sentence1 + sentence2;  //Telling the HtML page to add togther those two elements, when the funtion is performed//
 }
