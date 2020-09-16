a6ae7017f9011fe7d40ebf5db99199e01990ff01
jlynch725 <70283169+jlynch725@users.noreply.github.com>
2020-09-16 10:37:57 -0600

    Project6

--- Basic_Javascript_Projects/Project6_ternary_operators_constructors/JS/main.js
+++ Basic_Javascript_Projects/Project6_ternary_operators_constructors/JS/main.js
@@ -11,3 +11,35 @@ function vote_Function() {
     Can_vote = (Age < 18) ? "You are too young":"You are old enough";
     document.getElementById("vote").innerHTML = Can_vote + " to vote.";
 }
+
+function Vehicle(Make, Model, Year, Color) {
+    this.Vehicle_Make = Make;
+    this.Vehicle_Model = Model;
+    this.Vehicle_Year = Year;
+    this.Vehicle_Color = Color;
+}
+var Jack = new Vehicle("Dodge", "Viper", 2020, "Red");
+var Emily = new Vehicle("Jeep", "Trail Hawk", 2019, "White and Black");
+var Erik = new Vehicle("Ford", "Pinto", 1971, "Mustard");
+function myFunction() {
+    document.getElementById("Keywords_and_Constructors").innerHTML = "Erik drives a" + Erik.Vehicle_Color + "-colored " + Erik.Vehicle_Model + "manufactured in " + Erik.Vehicle_Year; 
+}
+var Tom = new Vehicle("Ford", "Edge", 2018, "Black");
+function newfunction() {
+    document.getElementById("New_and_This").innerHTML = "Tom Drives a "+ Tom.Vehicle_Color +"-colored " + Tom.Vehicle_Model + " manufactured in " + Tom.Vehicle_Year;
+}
+
+//var true = 5
+function reserved_word_function() {
+    document.getElementById("reserved_word").innerHTML = true
+}
+
+function count_Function() {
+    document.getElementById("Nested").innerHTML = Nested_Function(); 
+    function Nested_Function() {
+        var starting_point = 9;
+        function Plus_one() {starting_point += 1;}
+        Plus_one();
+        return starting_point;
+    }
+}
